5465152d3face47d94ab0bda063f75ef072cd496
Godswilll <anekwegodswill@gmail.com>
2022-09-14 17:17:22 -0700

    Commited to the course

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabeth - print alphabeth
+ *
+ * Return: void
+ */
+void print_alphabeth(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabeth_x10 alphabeth 10 times
+ *
+ * Return: void
+ */
+
+void print alphabeth x10(void)
+{
+	int round = 0;
+	char letter = 'a';
+
+	while (round < 10)
+	{
+		letter = 'a';
+		while (letter <= 'z')
+		{
+			_putchar('\n');
+			letter++;
+		}
+		_putchar('\n');
+
+		round++;
+	}
+}
