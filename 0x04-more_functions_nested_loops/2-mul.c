8110e13ad092894bdceda6a64ab32957fed69bc2
Godswilll <anekwegodswill@gmail.com>
2022-09-22 21:43:12 -0700

    pointers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - multiles ints.
+ * @a: integer.
+ * @b: integer.
+ *
+ * Return: product.
+ */
+int mul(int a, int b)
+{
+	int produc;
+	
+	 product = a * b;
+	return (product);
+}
